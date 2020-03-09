f0dc8da0087575b05f27a1f8108981f8d492aa5e
tGulevich <t.ananich@gmail.com>
2020-03-09 23:51:44 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str;
+  let f = 0;
+  if (!Array.isArray(str)) {
+    arr = arr.split('');
+  }
+
+  for (let i = 0; i < bracketsConfig.length; i++ ) {
+    arr.forEach((el, index) => {
+      if (bracketsConfig[i][0] === el && bracketsConfig[i][1] === arr[index + 1]) {
+        arr.splice(index, 2);
+        f = 1;
+      }
+    });
+  }
+
+  console.log(arr.length);
+  if (arr.length > 1 && f === 1) {
+    check(arr, bracketsConfig);
+  }
+  if (arr.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+  
 }
